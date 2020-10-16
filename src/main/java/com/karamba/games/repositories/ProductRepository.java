4a34b3fb03a4685222e9d8985db81b706979ea88
kunica777 <kunica777@gmail.com>
2020-10-16 22:33:12 +0300

    v1

--- src/main/java/com/karamba/games/repositories/ProductRepository.java
+++ src/main/java/com/karamba/games/repositories/ProductRepository.java
@@ -1,9 +1,12 @@
 package com.karamba.games.repositories;
 
 import com.karamba.games.entities.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 import java.util.Optional;
@@ -16,4 +19,8 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     Optional<Product> findById(Long id);
     Optional<Product> findByName(String name);
     List<Product> findProductsByNameContains(String name);
+
+
+    Page<Product> findByCategoryId(@RequestParam("id")Long id, Pageable pageable);
+    Page<Product> findByNameContaining(@RequestParam("name") String name, Pageable pageable);
 }
